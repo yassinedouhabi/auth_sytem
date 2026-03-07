1dbb4962d46acdc55a1920dfae7946d611072ef1
thepenguinboss <douhabiyassine@gmail.com>
2026-03-07 00:04:13 +0000

    add user model

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    const db = await mongoose.connect(process.env.MONGO_URL);
+    const db = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB connected: ${db.connection.host}`);
   } catch (error) {
     console.error(`MongoDB connection error: ${error.message}`);
